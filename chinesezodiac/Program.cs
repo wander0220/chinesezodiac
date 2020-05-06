ff376eb2bdd9e500946e99fc850f374da6be5bf8
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-05-06 15:37:36 +0900

    5번 문제 풀이 아 또 TMI지만 저는 무교입니다.

--- chinesezodiac/Program.cs
+++ chinesezodiac/Program.cs
@@ -10,6 +10,53 @@ namespace chinesezodiac
     {
         static void Main(string[] args)
         {
+
+            Console.Write("어느 해에 태어나셨죠? : ");
+            int year = int.Parse(Console.ReadLine());
+
+            switch (year % 12)
+            {
+                case 0:
+                    Console.WriteLine("원숭이");
+                    break;
+                case 1:
+                    Console.WriteLine("닭");
+                    break;
+                case 2:
+                    Console.WriteLine("개");
+                    break;
+                case 3:
+                    Console.WriteLine("돼지");
+                    break;
+                case 4:
+                    Console.WriteLine("소");
+                    break;
+                case 5:
+                    Console.WriteLine("개");
+                    break;
+                case 6:
+                    Console.WriteLine("호랑이");
+                    break;
+                case 7:
+                    Console.WriteLine("토끼");
+                    break;
+                case 8:
+                    Console.WriteLine("용");
+                    break;
+                case 9:
+                    Console.WriteLine("뱀");
+                    break;
+                case 10:
+                    Console.WriteLine("말");
+                    break;
+                case 11:
+                    Console.WriteLine("양");
+                    break;
+                default:
+                    Console.WriteLine("당신...혹시...하나님...?");
+                    break;
+            }
+
         }
     }
 }
